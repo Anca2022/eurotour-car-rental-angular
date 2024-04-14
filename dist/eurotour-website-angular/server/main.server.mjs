d85505b555a49b22d3da5540a1a3180ab40c8951
Anca Matei <ancamatei2012@gmail.com>
2024-04-14 22:02:22 +0300

    arrow-up component not needed

---
+++ dist/eurotour-website-angular/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-RAJGB7NW.mjs";import"./chunk-IZO4Y3YE.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { ArrowUpComponent } from './components/arrow-up/arrow-up.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FooterComponent, NavbarComponent, ArrowUpComponent],
+  imports: [CommonModule, RouterOutlet, FooterComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
