79d400dc7aed8a0eebb4f529177b146b61e619e7
Anca Matei <ancamatei2012@gmail.com>
2024-05-10 09:59:21 +0300

    updated package.json dependencies

--- dist/eurotour-website-angular/server/main.server.mjs
+++ dist/eurotour-website-angular/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-VNKPE3WP.mjs";import"./chunk-IZO4Y3YE.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-JSKBQVKD.mjs";import"./chunk-YUIGEO6X.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
